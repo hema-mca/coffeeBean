96d795cef4d4e0b669bbcbbc9824fc1fb9c0f486
saich-cendyn <csai@cendyn.com>
2024-02-23 14:56:44 +0530

    removed paginated data from event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user
 
   validates :type, presence: true
-
-  self.per_page = 10
-
+  
   def self.search(search)
     if(search)
       @events = self.where(type: search)
